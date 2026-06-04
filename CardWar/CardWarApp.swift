35b6e60c3383bbcf501959893228be35d7afb101
ldavidi <159155277+ldavidi@users.noreply.github.com>
2026-06-04 10:47:43 +0300

    Initial Commit

---
+++ CardWar/CardWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWarApp.swift
+//  CardWar
+//
+//  Created by Lior Gavriel Davidi on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CardWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardWar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardWar
+//
+//  Created by Lior Gavriel Davidi on 04/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
